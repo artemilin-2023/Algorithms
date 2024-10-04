743be777dc8bdd300791b8507781aeef08a3ea1b
LightChimera <artemilin347@gmail.com>
2024-10-04 15:46:58 +0300

    Реализовать сортировку слиянием

---
+++ Algorithms/Sorting.cs
@@ -0,0 +1,98 @@
+﻿namespace Algorithms
+{
+    public static class Sorting
+    {
+        public static IList<T> MergeSort<T>(this IList<T> array)
+            where T: IComparable
+        {
+            MergeSort(array, 0, array.Count - 1);
+            return array;
+        }
+
+        private static void MergeSort<T>(IList<T> array, int startIndex, int endIndex)
+            where T: IComparable
+        {
+            if (endIndex <= startIndex)
+                return;
+
+            var centerIndex = (startIndex + endIndex) / 2;
+            MergeSort(array, startIndex, centerIndex);
+            MergeSort(array, centerIndex + 1, endIndex);
+            Merge(array, startIndex, centerIndex, endIndex);
+        }
+
+        private static void Merge<T>(IList<T> array, int startIndex, int centerIndex, int endIndex)
+            where T: IComparable
+        {
+            var left = startIndex;
+            var right = centerIndex + 1; // Центральный элемент не включается в правую часть
+            var mergedArray = new T[endIndex - startIndex + 1];
+
+            var index = 0;
+            while (left <= centerIndex && right <= endIndex)
+            {
+                if (array[left].CompareTo(array[right]) < 0)
+                {
+                    mergedArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    mergedArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (int i = left; i <= centerIndex; i++) 
+            {
+                mergedArray[index] = array[i];
+                index++;
+            }
+            for (int i = right; i <= endIndex; i++)
+            {
+                mergedArray[index] = array[i];
+                index++;
+            }
+
+            // Заменяем элементы исходного массива
+            for (int i = 0; i < mergedArray.Length; i++)
+            {
+                array[startIndex + i] = mergedArray[i];
+            }
+        }
+
+        public static IList<T> QuickSort<T>(this IList<T> array)
+            where T: IComparable
+        {
+            QuickSort(array, 0, array.Count - 1);
+            return array;
+        }
+
+        private static void QuickSort<T>(IList<T> array, int left, int right)
+            where T: IComparable
+        {
+            if (right <= left)
+                return;
+
+            var pivot = array[left];
+            var i = left;
+            var j = right;
+            while (i <= j)
+            {
+                while (array[i].CompareTo(pivot) < 0) i++;
+                while (array[j].CompareTo(pivot) > 0) j--;
+                if (i <= j)
+                {
+                    (array[j], array[i]) = (array[i], array[j]);
+                    i++;
+                    j--;
+                }
+            }
+
+            QuickSort(array, left, j);
+            QuickSort(array, i, right);
+        }
+    }
+}
---
+++ AlgorithmsTests/SortingTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Algorithms.Tests
+{
+    [TestClass()]
+    public class SortingTests
+    {
+        [TestMethod()]
+        public void MergeSortTest_SmallArray()
+        {
+            var actual = new[] { 1, 3, 2, 5, 4 };
+            actual.MergeSort();
+            CollectionAssert.AreEqual(new[] { 1, 2, 3, 4, 5 }, actual);
+        }
+
+        [TestMethod()]
+        public void MergeSortTest_EmptyArray()
+        {
+            var actual = new int[0];
+            actual.MergeSort();
+            CollectionAssert.AreEqual(new int[0], actual);
+        }
+
+        [TestMethod()]
+        public void MergeSortTest_LargeArray()
+        {
+            const int elementsAmount = 100000000;
+            var actual = new List<int>();
+            var expected = new List<int>();
+            var random = new Random();
+            for (int i = 0; i < elementsAmount; i++)
+            {
+                var item = random.Next(i);
+                actual.Add(item);
+                expected.Add(item);
+            }
+
+            Sorting.MergeSort(actual);
+            expected.Sort();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void QuickSortTest_SmallArray()
+        {
+            var result = Sorting.QuickSort(new List<int> { 3, 1, 2, 5 });
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 5}, (ICollection)result);
+        }
+
+        [TestMethod()]
+        public void QuickSortTest_EmptyArray()
+        {
+            var result = Sorting.QuickSort(new List<int>());
+            CollectionAssert.AreEqual(new List<int>(), (ICollection)result);
+        }
+
+        [TestMethod()]
+        public void QuickSortTest_LargeArray()
+        {
+            const int elementsAmount = 100000000;
+            var actual = new List<int>();
+            var expected = new List<int>();
+            var random = new Random();
+            for (int i = 0; i < elementsAmount; i++)
+            {
+                var item = random.Next(i);
+                actual.Add(item);
+                expected.Add(item);
+            }
+
+            Sorting.QuickSort(actual);
+            expected.Sort();
+            
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}

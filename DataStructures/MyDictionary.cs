d4602ab0637c5920abd4ac11e4315c1c8a1717a3
LightChimera <artemilin347@gmail.com>
2024-10-06 20:28:08 +0300

    Реализовать хеш таблицу

---
+++ DataStructures/MyDictionary.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DataStructures
+{
+    public class MyDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IEnumerable<KeyValuePair<TKey, TValue>>, ICollection<KeyValuePair<TKey, TValue>>
+    {
+
+        public ICollection<TKey> Keys => buckets.SelectMany(b => b.Select(i => i.Key)).ToList();
+        public ICollection<TValue> Values => buckets.SelectMany(b => b.Select(i => i.Value)).ToList();
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (TryGetValue(key, out var value))
+                    return value;
+                throw new ArgumentException($"По ключу {key} не удалось получить значение.");
+            }
+
+            set
+            {
+                if (!TryInsert(key, value))
+                    Add(key, value);
+            }
+        }
+
+        private const int MaxLoadFacotr = 3;
+        private const int MinLoadFactor = 1;
+        private const int InitialSize = 5;
+
+        private List<KeyValuePair<TKey, TValue>>[] buckets;
+        private int Size => buckets.Length;
+        private int LoadFactor => Count / Size;
+        private bool NeedResizing => LoadFactor > MaxLoadFacotr || LoadFactor < MinLoadFactor && Size > InitialSize;
+
+        public MyDictionary()
+        {
+            buckets = new List<KeyValuePair<TKey, TValue>>[InitialSize];
+            InitializeBuckets(InitialSize, buckets);
+            Count = 0;
+        }
+
+        private void InitializeBuckets(int size, List<KeyValuePair<TKey, TValue>>[] array)
+        {
+            for (int i = 0; i < size; i++)
+                array[i] = new List<KeyValuePair<TKey, TValue>>();
+        }
+
+        private bool TryInsert(TKey key, TValue value)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+
+            var bucket = buckets[GetBucket(key, Size)];
+            for (int i = 0; i < bucket.Count; i++)
+            {
+                if (bucket[i].Key!.Equals(key))
+                {
+                    bucket[i] = new KeyValuePair<TKey, TValue>(key, value);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Add(TKey key, TValue value)
+            => Add(new KeyValuePair<TKey, TValue>(key, value));
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            ArgumentNullException.ThrowIfNull(item.Key);
+            if (ContainsKey(item.Key))
+                throw new ArgumentException($"Элемент с ключем {item.Key} уже добавлен.");
+
+            if (NeedResizing)
+                ResizeTable();
+
+            var index = GetBucket(item.Key, Size);
+            buckets[index].Add(item);
+            Count++;
+        }
+
+        private void ResizeTable()
+        {
+            var newSize = LoadFactor > MaxLoadFacotr ? Size * 2 : Size / 2;
+            var newBuckets = new List<KeyValuePair<TKey, TValue>>[newSize];
+            InitializeBuckets(newSize, newBuckets);
+
+            foreach (var bucket in buckets)
+            {
+                foreach(var item in bucket)
+                {
+                    var index = GetBucket(item.Key, newSize);
+                    newBuckets[index].Add(item); 
+                }
+            }
+
+            buckets = newBuckets;
+        }
+
+        private int GetBucket(TKey key, int size) 
+            => Math.Abs(key!.GetHashCode() % size);
+
+        public void Clear()
+        {
+            Array.Clear(buckets, 0, buckets.Length);
+            Count = 0;
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            ArgumentNullException.ThrowIfNull(item.Key);
+
+            var index = GetBucket(item.Key, Size);
+            foreach (var pair in buckets[index])
+            {
+                if (pair.Equals(item)) 
+                    return true;
+            }
+            return false;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+
+            var index = GetBucket(key, Size);
+            foreach (var item in buckets[index])
+            {
+                if (item.Key!.Equals(key))
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array)
+            => CopyTo(array, 0);
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            ArgumentNullException.ThrowIfNull(array);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(arrayIndex, array.Length - 1);
+            ArgumentOutOfRangeException.ThrowIfNegative(arrayIndex);
+            if (array.Length - arrayIndex < Count)
+                throw new ArgumentException("Размерности массива недостаточно для копирования всех элементов.");
+
+            var i = arrayIndex;
+            foreach (var bucket in buckets)
+            {
+                foreach (var item in bucket)
+                {
+                    array[i++] = item;
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            => buckets.SelectMany(b => b.Select(i => i)).GetEnumerator();
+
+        public bool Remove(TKey key)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+
+            var bucket = buckets[GetBucket(key, Size)];
+            for (var i = 0; i < bucket.Count; i++)
+            {
+                if (bucket[i].Key!.Equals(key))
+                {
+                    bucket.RemoveAt(i);
+                    Count--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+            => Remove(item.Key);
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            ArgumentNullException.ThrowIfNull(key);
+            value = default(TValue);
+
+            var index = GetBucket(key, Size);
+            foreach (var item in buckets[index])
+            {
+                if (item.Key!.Equals(key))
+                {
+                    value = item.Value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ DataStructuresTests/MyDictionaryTests.cs
@@ -0,0 +1,236 @@
+﻿using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+/*
+ * Человек, что смотрит эти тесты, зачем ты тут? В любом случае, держи арт
+ * 
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣯⣾⣿⡿⢟⣿⠛⠉⠩⠁⠀⠀⡟⠁⠀⣀⠀⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠈⠢⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⡿⠟⠁⣐⡮⠁⠀⡐⠀⠀⠀⠀⢰⣅⠈⠀⠒⠄⡀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠐⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣰⡿⠋⠀⠀⠀⡬⠁⠀⡐⠀⠀⠀⠀⠀⠀⠟⢂⠀⠀⠀⠈⠂⡀⠀⠀⠀⠈⠙⢿⣿⣻⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣲⠏⠀⠀⠀⠠⡙⠀⠀⢀⠁⠀⠀⠀⠀⠀⠀⠐⡈⠑⡀⠀⠀⠀⠈⠢⡀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⢗⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠬⠁⠀⢀⠈⠀⢠⠁⠀⠀⡈⠀⠀⠀⠀⠀⠀⠀⠀⠐⡀⠈⢄⠀⠀⠀⠀⠀⢄⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠈⠦⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠁⠀⠀⠂⠀⠀⡄⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠈⢂⠀⠀⠀⠀⠀⢂⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣗⡀⠀⠀⠀⠀⠀⠈⣇⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⣨⠁⠀⠀⠄⠀⠀⢀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢂⠀⠀⠀⠀⠀⢂⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡰⠀⠀⠀⠀⠀⠀⢩⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⡘⠀⠀⠀⢸⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⣣⠀⠀⠀⠀⠀⠀⠀⠀⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡇⢃⠀⠀⠀⠀⠀⠸⢆⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⣨⠀⠀⢀⠁⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⢁⢀⠀⠀⠀⠀⠀⠀⠀⠈⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⡇⠋⡀⠀⠀⠀⠀⠀⣏⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⡈⠀⠀⠀⠀⠀⠀⠰⢰⣿⡆⠀⠀⠀⠀⠀⠀⠄⢂⠀⠐⡀⠀⠀⠀⠀⠈⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠈⢡⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⢨⠀⠀⢀⠁⠀⠀⠀⠀⢀⠀⠆⣿⣿⣧⠀⠠⠀⠐⠀⠀⠘⡀⠠⠀⠐⣀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠈⡄⠀⠀⠀⠀⠘⠆⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⣿⠀⠀⠘⠀⠀⠀⠀⠀⠸⠰⢸⣿⣿⣿⣆⠀⢂⠀⠀⠀⠀⠐⡀⠐⠀⠐⠑⠀⠀⠈⠄⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⢁⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⡊⠀⠀⡀⠀⠀⠀⠀⠀⢐⢳⣾⣿⣿⣿⣿⡌⠌⣆⠀⠀⠀⠀⠐⠀⠈⠂⠈⢀⠢⡀⠀⢂⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⠘⠀⠀⠀⠀⠀⠣⠀⠀⠀⠀
+ *  ⠀⠀⠀⠀⠀⠀⡅⠀⠀⡇⠀⡀⠀⠀⠀⢰⣧⣿⣿⣿⣿⣿⣿⣄⠚⢆⠀⠀⠀⠀⠈⢄⠀⠑⠀⠁⡀⠀⠀⠂⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⡆⠀⠀⡆⡆⡔⠄⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⣿⠀⠃⢸⢀⡇⠀⠀⠠⠈⣻⣿⣿⣿⣿⣿⣿⣿⣦⠙⡧⡀⠀⠀⠀⠀⠀⡀⠀⠀⠈⠀⡀⢀⢲⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⢸⠆⠀⠀⡇⢰⠀⢳⠁⢷⠗⡀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⢹⢰⠀⠈⣸⢰⠀⠀⠀⠒⡹⣿⣿⣿⣿⣿⣿⢿⣿⣷⣜⣮⣦⠀⠀⠀⠀⠈⠊⠀⠀⠀⢄⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢤⢸⠀⠀⠀⡇⢸⢰⣾⢠⠆⠁⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠸⢸⠆⠀⢡⢀⠃⠀⠀⢀⠱⡹⣿⣿⣿⣿⣯⣿⣼⣹⣻⣮⣿⣿⡦⡀⠐⠄⠀⠈⠢⡢⢀⠀⠠⡣⡀⠀⠀⠀⠀⠀⠀⠀⡿⢸⠀⠀⠀⡇⠸⢘⠙⣾⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⢸⣾⣯⣶⠀⢸⠎⠄⡀⠈⡷⣼⣌⢻⣿⣿⣿⠿⢻⠩⠁⠀⠈⠑⢖⠠⠔⢄⠀⢌⠐⠨⠢⠁⠣⠂⠰⢠⣄⠀⠀⠀⠀⢠⡙⡈⠀⠀⠀⣌⢀⢸⠀⠏⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⢹⣁⠱⡇⢸⢰⡘⢇⢄⢡⠈⢿⣷⠟⠋⠀⠀⠀⢅⠀⠀⠀⠀⠀⠁⠀⠀⠈⠂⡝⡰⢠⠄⣐⡆⠀⢀⠀⠈⠀⠂⢠⢲⢡⠃⠀⠀⣰⠂⣸⡆⠘⠃⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⢩⣆⢼⢡⠈⠂⠱⢕⠌⡅⠀⢀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠂⠁⠊⠔⣣⣀⣀⣀⠸⠀⣠⢏⡲⡉⠀⠠⢠⡏⢠⣗⠁⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⢯⣛⣥⠀⠀⠂⠈⠩⢊⠀⠀⠀⠀⠀⢀⡀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢁⣵⠉⠁⠀⠄⡆⡊⣜⡚⡔⠀⢠⡵⣿⢠⠯⠂⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⡎⠙⢓⠄⠀⢆⠀⠑⠱⢄⠀⠀⠀⠁⢃⡀⠀⠉⠀⠀⠀⠀⠀⠀⢀⢐⠚⠹⠀⡀⣨⢰⡀⢨⡣⡞⣠⡰⠉⢀⣭⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠤⠢⠽⠦⢈⠑⢦⢀⠳⡅⡠⡀⠀⠀⠀⠀⠀⠁⠀⠀⠀⡠⣐⠁⡂⢀⢃⣔⣔⣇⢛⢣⡜⣷⠮⡧⠃⠈⢐⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⡆⠀⢠⠈⠑⠊⣾⣺⢳⣿⠙⢲⣄⡀⠀⢀⣀⣤⡔⢻⡔⠃⠁⣀⡔⢹⡏⢹⣿⠁⠙⣷⣟⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⠀⠀⠀⡄⣴⠾⠟⣴⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠟⠃⣿⣿⡅⠘⠈⠉⠉⠁⠀⠀⠀⠃⠛⠃⢸⣷⣼⡏⠀⠀⠈⠻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠀⠀⠀⣠⠤⢓⣵⣾⣿⣷⣄⠈⢘⠲⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣷⣶⣶⣶⣤⣤⣤⣤⣶⣶⣶⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⢀⡤⠓⠁⣰⡿⣽⢾⣟⣿⣿⣧⡄⡘⠢⡟⣧⣠⡀⠀⠀⢀⡀⣀⣴⣁⠟⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ *   ⠟⠁⢀⣾⢯⣟⣽⣻⣞⡿⣿⣿⣿⣮⣳⣜⣻⣿⣷⠒⠊⠉⠁⣠⣴⣶⣦⣤⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣗⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⢤⣤⣠⠀⠀⠀⠀⠀
+ *   ⠀⡠⢟⣞⣯⣞⣷⣻⣾⣿⣿⣿⣿⣿⣿⣾⣷⣿⢿⣵⣤⣶⣿⣿⡿⣿⣿⣿⣿⣾⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡴⣠⠀⢀⣠⠠⠒⠒⠒⠒⠋⣰⣿⡿⣿⣷⣴⠀⠀⠀
+ *   ⢀⠝⣸⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡐⣉⠯⣄⠀⠀⠀⠀⠀⣴⣿⢯⣿⢷⣿⣿⣷⡂⠀
+ *   ⢈⢠⣿⣽⣻⣿⢿⣯⣿⣽⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣵⣫⡽⣷⠀⢀⣠⣾⡿⣯⣿⣾⣿⣿⣟⣿⣿⡄
+ *   ⠀⣼⣷⣻⣿⣾⣿⣯⣿⣯⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⡽⣧⠛⠿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿
+ *   ⢠⣿⡽⣟⣷⣿⣿⣿⣷⣿⡿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣬⣟⣯⣿⣿⣿⣿⣿⣿⣿⣿
+ *
+ * 
+ * 
+ * А теперь иди своей дорогой.
+ * 
+ */
+
+namespace DataStructures.Tests
+{
+    [TestClass()]
+    public class MyDictionaryTests
+    {
+        private MyDictionary<string, int> table;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            table = new MyDictionary<string, int>();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            table.Add("one", 1);
+            table.Add("two", 2);
+            table.Add("three", 3);
+
+            Assert.AreEqual(3, table.Count);
+        }
+
+        [TestMethod()]
+        public void AddTest_KeyAlreadyExist()
+        {
+            table.Add("one", 1);
+
+            Assert.ThrowsException<ArgumentException>(() => table.Add("one", 1));
+        }
+
+        [TestMethod()]
+        public void AddTest_ResizingTest()
+        {
+            var keys = new List<string>();
+            var values = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                keys.Add(i.ToString());
+                values.Add(i);
+                table.Add(i.ToString(), i);
+            }
+
+            Assert.AreEqual(100, table.Count);
+            CollectionAssert.AreEqual(keys.Order().ToList(), table.Keys.Order().ToList());
+            CollectionAssert.AreEqual(values, table.Values.Order().ToList());
+        }
+
+        [TestMethod()]
+        public void GetSetTest()
+        {
+            table["one"] = 0;
+            table["two"] = 2;
+
+            table["one"]++;
+
+            Assert.AreEqual(1, table["one"]);
+            Assert.AreEqual(2, table["two"]);
+        }
+
+        [TestMethod()]
+        public void GetSetTest_TryGetValue()
+        {
+            table["one"] = 0;
+            table["two"] = 2;
+
+            Assert.ThrowsException<ArgumentException>(() => table["three"]);
+        }
+
+        [TestMethod()]
+        public void EnumeratorTest()
+        {
+            table["one"] = 0;
+            table["two"] = 1;
+            table["three"] = 2;
+
+            foreach (var item in table)
+            {
+                Assert.AreEqual(table[item.Key], item.Value);
+            }
+        }
+
+        [TestMethod()]
+        public void ContainsTest_True()
+        {
+
+            table["one"] = 0;
+            table["two"] = 1;
+            table["three"] = 2;
+
+            Assert.IsTrue(table.Contains(new KeyValuePair<string, int>("one", 0)));
+            Assert.IsTrue(table.Contains(new KeyValuePair<string, int>("two", 1)));
+            Assert.IsTrue(table.Contains(new KeyValuePair<string, int>("three", 2)));
+        }
+
+        [TestMethod()]
+        public void ContainsTest_False()
+        {
+
+            table["one"] = 0;
+            table["two"] = 1;
+            table["three"] = 2;
+
+            Assert.IsFalse(table.Contains(new KeyValuePair<string, int>("one1", 0)));
+            Assert.IsFalse(table.Contains(new KeyValuePair<string, int>("two", 2)));
+            Assert.IsFalse(table.Contains(new KeyValuePair<string, int>("t", -2)));
+        }
+
+        [TestMethod()]
+        public void ContainsTest_NullKey()
+        {
+
+            table["one"] = 0;
+            table["two"] = 1;
+            table["three"] = 2;
+
+            Assert.ThrowsException<ArgumentNullException>(() => table.Contains(new KeyValuePair<string, int>(null, 0)));
+        }
+
+        [TestMethod()]
+        public void CopyToTest_CopyAll()
+        {
+            table["one"] = 1;
+            table.Add("two", 2);
+            table.Add("three", 2);
+
+            var array = new KeyValuePair<string, int>[3];
+            table.CopyTo(array, 0);
+
+            CollectionAssert.AreEqual(table.Values.Order().ToList(), array.Select(i => i.Value).Order().ToList());
+        }
+
+        [TestMethod()]
+        public void CopyToTest_ArrayBigger()
+        {
+            table["one"] = 1;
+            table.Add("two", 2);
+            table.Add("three", 2);
+
+            var array = new KeyValuePair<string, int>[4];
+            table.CopyTo(array, 1);
+
+            CollectionAssert.AreEqual(table.Values.Order().ToList(), array.TakeLast(3).Select(i => i.Value).Order().ToList());
+        }
+
+        [TestMethod()]
+        public void CopyToTest_ArrayLess()
+        {
+            table.Add("Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно Длани Господней парящей над миром? По крайне мере истинно то, что человек не властен даже над своей волей.", 2);
+            table["one"] = 1;
+            table.Add("two", 2);
+
+            var array = new KeyValuePair<string, int>[2];
+
+            Assert.ThrowsException<ArgumentException>(() => table.CopyTo(array, 0));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            table.Add("one", 1);
+            table.Add("two", 2);
+            table.Add("imposter", -1);
+
+            Assert.AreEqual(3, table.Count);
+
+            table.Remove("imposter");
+            Assert.AreEqual(2, table.Count);
+            CollectionAssert.AreEqual(new List<int>() { 1, 2 }, table.Values.Order().ToList());
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            for (int i = 0; i < 100; i++)
+                table.Add(i.ToString(), i);
+
+            table.Clear();
+            Assert.AreEqual(table.Count, 0);
+        }
+    }
+}

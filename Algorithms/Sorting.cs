ec1571540d44afa97dc19c25c668f472666b1989
LightChimera <artemilin347@gmail.com>
2024-10-16 21:45:46 +0300

    Добавить оглавление на будущие разделы

--- Algorithms/Sorting.cs
+++ Algorithms/Sorting.cs
@@ -150,9 +150,9 @@
         {
             for (int i = 0; i < array.Count - 1; i++) 
             {
-                for (int j = i + 1; j < array.Count; j++)
+                for (int j = 0; j < array.Count - i - 1; j++)
                 {
-                    if (array[j - 1].CompareTo(array[j]) > 0)
+                    if (array[j].CompareTo(array[j + 1]) > 0)
                         Swap(array, j - 1, j);
                 }
             }
